9d8bd629482f851e8428ee2bd09595cc6e419886
VOsipov <VOsipov@kdl.ru>
2025-11-25 12:40:00 +0300

    9.6.1 (HW-03) Complete

---
+++ Exeption/LearnException.cs
@@ -0,0 +1,6 @@
+﻿namespace Exeption;
+
+internal class LearnException : Exception
+{
+    public LearnException(String message) : base(message) { }
+}
--- Exeption/Program.cs
+++ Exeption/Program.cs
@@ -1,2 +1,36 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿namespace Exeption;
+
+class Program
+{
+    private static List<Exception> exceptions = new List<Exception>();
+
+    static void Main(string[] args)
+    {
+        CreateExceptionList();
+        WriteExceptionMessage();
+    }
+
+    static void CreateExceptionList()
+    {
+        exceptions.Add(new Exception("Исключение Exception"));
+        exceptions.Add(new FormatException("Исключение FormatException"));
+        exceptions.Add(new NotSupportedException("Исключение NotSupportedException"));
+        exceptions.Add(new KeyNotFoundException("Исключение KeyNotFoundException"));
+        exceptions.Add(new LearnException("Исключение LearnException"));
+    }
+
+    static void WriteExceptionMessage()
+    {
+        foreach (var exception in exceptions)
+        {
+            try
+            {
+                throw exception;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
